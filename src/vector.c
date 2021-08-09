ba67be02b38338cf86a170f7257ac14d782a2489
Origami404 <Origami404@foxmail.com>
2021-08-09 19:24:41 +0800

    Add: vector

---
+++ src/vector.c
@@ -0,0 +1,104 @@
+#define VECTOR_DEF 1
+#include "vector.h"
+
+typedef struct vector {
+    void *data;
+    size_t length;
+    size_t size;
+
+    size_t rlen;
+} vector;
+
+vector* vec_create(data_spec spec) {
+    vector *v = checked_malloc(sizeof(*v));
+
+    spec.padding = spec.padding ? spec.padding : spec.size;
+
+    // FIX: mul overflow
+    v->data = checked_malloc(spec.size * spec.length); 
+    void *end = spec.data + (spec.padding * spec.length);
+
+    void *dst = v->data;
+    for (void *src = spec.data; src < end; src += spec.padding) {
+        memcpy(dst, src, spec.size);
+        dst += spec.size;
+    }
+
+    v->length = spec.length;
+    v->size = spec.size;
+    v->rlen = spec.length;
+
+    return v;
+}
+
+void vec_ensure_fit(vector *v, vector *o) {
+    if (v->size != o->size) {
+        panic("Size unfit: %u %u", v->size, o->size);
+    }
+}
+
+void vec_extend_to(vector *v, size_t len) {
+    if (v->rlen < len) {
+        checked_realloc(v->data, len);
+        v->rlen = len;
+    }
+}
+
+void* vec_end(vector *v) {
+    return (char*)v->data + v->length;
+}
+
+void vec_concat_cpy(vector *v, vector *o) {
+    vec_ensure_fit(v, o);
+
+    v->length += o->length;
+    vec_extend_to(v, v->length);
+
+    memcpy(vec_end(v), o->data, o->length * o->size);
+}
+
+void vec_concat_mov(vector *v, vector *o) {
+    vec_ensure_fit(v, o);
+
+    v->length += o->length;
+    vec_extend_to(v, v->length);
+
+    memmove(vec_end(v), o->data, o->length * o->size);
+}   
+
+void vec_add(vector *v, void *data) {
+    if (v->rlen == v->length) {
+        vec_extend_to(v, v->rlen * 2);
+    }
+
+    memcpy(vec_end(v), data, v->size);
+    v->length += 1;
+}
+
+void vec_pop(vector *v) {
+    v->length -= 1;
+}
+
+void* vec_get(vector *v, int idx) {
+    const int len = (int)v->length;
+
+    if (-idx > len || idx >= len) {
+        panic("Out of index: %d(len: %d)", idx, len);
+    }
+
+    if (idx < 0)
+        idx += len;
+
+    return (char)v->data + idx;
+}
+
+void vec_set(vector *v, int idx, void *data) {
+    void *p = vec_get(v, idx);
+    memcpy(p, data, v->size);
+}
+
+void vec_destory(vector *v) {
+    free(v->data);
+    free(v);
+}
+
---
+++ src/vector.h
@@ -0,0 +1,28 @@
+#ifndef HEADER_VECTOR_H__
+#define HEADER_VECTOR_H__
+
+#include "util.h"
+
+#ifndef VECTOR_DEF
+typedef void vector;
+#endif // VECTOR_DEF
+
+typedef struct data_spec {
+    void *data;       // pointer to the data
+    size_t size;      // the size of each element, in byte
+    size_t length;    // how many element need to copy
+    size_t padding;   // the padding between each element, in byte
+} data_spec;
+
+vector* vec_create(data_spec spec);
+
+void  vec_concat_cpy(vector *v, vector *other);
+void  vec_concat_mov(vector *v, vector *other);
+void  vec_add(vector *v, void *data);
+void  vec_pop(vector *v);
+void const* vec_get(vector *v, int idx);
+void  vec_set(vector *v, int idx, void *data);
+
+void vec_destory(vector *v);
+
+#endif // HEADER_VECTOR_H__
