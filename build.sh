e861ddc6e04ca9cfe71163fe6fe4a3a9b8674f91
Origami404 <Origami404@foxmail.com>
2021-08-15 08:20:31 +0800

    Add: clear-up, debug mode

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!bash
 cd /home/origami/code/bisheng/SysY2021/src
 
+bash ../clear.sh
+
 cd frontend
 flex lexer.lex
 bison -d parser.y 
@@ -8,10 +10,17 @@ cd ..
 
 python ../scripts/ast_gen.py
 
+if [ $1 == "debug" ]
+then
+    addition_flag="-Wall -g -DDEBUG"
+else
+    addition_flag="-O2"
+fi
+
 # ref: https://unix.stackexchange.com/a/19656
 for c_file in $(find . | grep ".*\.c$") 
 do
-    gcc -std=c11 -Wall -c -I$PWD -o ${c_file%.c}.o $c_file
+    gcc -std=c11 ${addition_flag} -c -I$PWD -o ${c_file%.c}.o $c_file
 done
 
-gcc -std=c11 -Wall $(find . | grep ".*\.o$") -o ../ssyc
+gcc -std=c11 ${addition_flag} $(find . | grep ".*\.o$") -o ../ssyc
