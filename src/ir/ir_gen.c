dd634055b26e1f09c6f74281290163649ffb50f3
Origami404 <Origami404@foxmail.com>
2021-10-23 21:07:46 +0800

    Change: ExpNode Add: Cond gen Add: StmtIf gen

--- src/ir/ir_gen.c
+++ src/ir/ir_gen.c
@@ -18,35 +18,33 @@
 
 // ===== 常量表达式求值 =====
 int eval_const_op(Ast_Node n) {
-    assert_type(n, AT_ExpOp);
+    assert_type(n, AT_ExpAdd);
 
-    const int a = eval_const_exp(n->exp_op.arg1);
-    const int b = eval_const_exp(n->exp_op.arg2);
+    const int a = eval_const_exp(n->exp_add.arg1);
+    const int b = eval_const_exp(n->exp_add.arg2);
 
-    switch (n->exp_op.op) {
+    switch (n->exp_add.op) {
         case OP_ADD: return a + b;
         case OP_SUB: return a - b;
         case OP_MUL: return a * b;
         case OP_DIV: return a / b;
         case OP_MOD: return a % b;
-        default: panic("Unsupport op: %d", n->exp_op.op);
+        default: panic("Unsupport op: %d", n->exp_add.op);
     }
 }
 
 int eval_const_exp(Ast_Node n) {
     assert_type_exp(n);
-    assert_type(n, AT_ExpOp);
-    assert_type(n, AT_ExpNum);
 
     switch (n->type) {
-        case AT_ExpOp:   return eval_const_op(n);
+        case AT_ExpAdd:  return eval_const_op(n);
         case AT_ExpNum:  return n->exp_num.val;
         case AT_ExpLval: return ir_info_var_get(n->exp_lval.lval->lval.name).init;
         default: panic("Unsupport type: %d", n->type);
     }
 }
 
-IR_Type op2ir_type(Ast_OpType t) {
+IR_Type op2ir_type(Ast_ExpAddType t) {
     switch (t) {
         case OP_ADD: return IRT_ADD;
         case OP_SUB: return IRT_SUB;
@@ -113,15 +111,17 @@ void tac_gen_exp_int(Ast_Node n) {
     // 这个函数只会在非常量表达式上被调用, which means 它不可能在全局生成 IR, 只能生成局部变量
 
     switch (n->type) {
-        case AT_ExpOp: {
-            tac_gen_exp_int(n->exp_op.arg1);
+        case AT_ExpAdd: {
+            // 正常的表达式生成不可能生成非 int 值的表达式
+            // Rel/Log 表达式只能在 if/while 的条件内出现
+            tac_gen_exp_int(n->exp_add.arg1);
             string const op1 = ir_now->dest;
 
-            tac_gen_exp_int(n->exp_op.arg2);
+            tac_gen_exp_int(n->exp_add.arg2);
             string const op2 = ir_now->dest;
 
             string const dest = ir_temporary();
-            ir_code_add(op2ir_type(n->exp_op.op), dest, op1, op2);
+            ir_code_add(op2ir_type(n->exp_add.op), dest, op1, op2);
         } break;
         
         case AT_ExpLval: {
@@ -175,168 +175,278 @@ void tac_gen_exp_int(Ast_Node n) {
 void tac_gen_block(Ast_Node n) {
     assert_type(n, AT_Block);
 
-    // TODO:
+    ir_info_scope_push();
+    for (Ast_List p = n->block.items; p; p = p->cdr) {
+        tac_gen_stmt(p->car);
+    }
+    ir_info_scope_pop();
 }
 
-void gen_stmt_and_fill_holes(Ast_Node n, vector_t(string *) *holes) {
+void tac_gen_stmt(Ast_Node n) {
+    assert_type_stmt(n);
+    // TODO
+    
+    switch (n->type) {
+        case AT_Block: tac_gen_block(n); break;
+        case AT_StmtIf: tac_gen_if(n); break;
+        case AT_StmtWhile: 
+        case AT_StmtAssign:
+        case AT_StmtBreak:
+        case AT_StmtContinue:
+        case AT_StmtReturn: 
+        case AT_StmtExp: tac_gen_exp_int(n->stmt_exp.exp); break;
+        case AT_StmtEmpty: break;
+        default: panic("Unkonwn stmt type: %d", n->type);
+    }
+}
+
+Ast_ExpRelType adjust_op(Ast_ExpRelType op, bool val_should_jmp) {
+    if (!val_should_jmp) {
+        switch (op) {
+            case OP_EQ:          return OP_NOT_EQ;
+            case OP_NOT_EQ:      return OP_EQ;
+            case OP_GREATER:     return OP_LESS_EQ;
+            case OP_GREATER_EQ:  return OP_LESS;
+            case OP_LESS:        return OP_GREATER_EQ;
+            case OP_LESS_EQ:     return OP_GREATER;
+            default: panic("Not a rel op: %d", op);
+        }
+    } else return op;
+}
+
+IR_Type rel2jmp(Ast_ExpRelType t) {
+    switch (t) {
+        case OP_EQ:          return IRT_JEQ;
+        case OP_NOT_EQ:      return IRT_JNE;
+        case OP_LESS:        return IRT_JLT;
+        case OP_LESS_EQ:     return IRT_JLE;
+        case OP_GREATER:     return IRT_JGT;
+        case OP_GREATER_EQ:  return IRT_JGE;
+        default: panic("Not an relation op: %d", t);
+    }
+}
+
+void cond_gen_rel(Ast_Node n, bool val_should_jmp, vector_t(string *) holes) {
+    // assert_type(n, AT_ExpRel);
+
+    if (n->type != AT_ExpRel) {
+        tac_gen_exp_int(n->exp_rel.arg1);
+        vec_add(holes, ir_code_add_with_undetermined_label(IRT_JNE, ir_now->dest, num2str(0)));
+        return;
+    } 
+
+    tac_gen_exp_int(n->exp_rel.arg1);
+    string const a = ir_now->dest;
+
+    tac_gen_exp_int(n->exp_rel.arg2);
+    string const b = ir_now->dest;
+
+    vec_add(holes, 
+        ir_code_add_with_undetermined_label(
+            rel2jmp(adjust_op(n->exp_rel.op, val_should_jmp)), a, b));
+    
+}
+
+void cond_gen_log(Ast_Node n, vector_t(string *) true_holes, vector_t(string *) false_holes) {
+    // assert_type(n, AT_ExpLog);
+
+    if (n->type != AT_ExpLog) {
+        // TODO: 消除 "跳到下一条指令" 这种无用的 JMP 指令
+        cond_gen_rel(n, true, true_holes);
+        return;
+    }
+
+    switch (n->exp_log.op) {
+        case OP_LOG_AND: {
+            cond_gen_rel(n->exp_log.arg1, false, false_holes);
+            cond_gen_log(n->exp_log.arg2, true_holes, false_holes);
+        } break;
+
+        case OP_LOG_OR: {
+            // TODO: 消除 "跳到下一条指令" 这种无用的 JMP 指令
+            cond_gen_rel(n->exp_log.arg1, true, true_holes);
+            cond_gen_log(n->exp_log.arg2, true_holes, false_holes);
+        } break;
+
+        case OP_LOG_NOT: {
+            // TODO: 消除 "跳到下一条指令" 这种无用的 JMP 指令
+            cond_gen_rel(n->exp_log.arg1, false, true_holes);
+        } break;
+
+        default: panic("Unkonwn log op type: %d", n->exp_log.op);
+    }
+}
+
+vector create_empty_label_hole(void) {
+    return vec_create((data_spec){ 0, 0, sizeof(string *), 0 });
+}
+
+void stmt_gen_and_fill_holes(Ast_Node n, vector_t(string *) holes) {
     assert_type_stmt(n);
 
     size_t const label = ir_now_offset + 1;
     tac_gen_stmt(n);
 
-    size_t const len = vec_len(*holes);
+    size_t const len = vec_len(holes);
     for (size_t i = 0; i < len; i++) {
-        *(string *)vec_get(*holes, i) = num2str(label);
+        *(string *)vec_get(holes, i) = num2str(label);
     }
 }
 
-IR_Type rel_op_to_negative_ir(Ast_OpType t) {
-    switch (t) {
-        case OP_EQ:          return IRT_JNE;
-        case OP_NOT_EQ:      return IRT_JEQ;
-        case OP_LESS:        return IRT_JGE;
-        case OP_LESS_EQ:     return IRT_JGT;
-        case OP_GREATER:     return IRT_JLE;
-        case OP_GREATER_EQ:  return IRT_JLT;
-        default: panic("Not an relation op: %d", t);
-    }
-}
-
-void gen_exp_cond_with_jmp(Ast_Node exp, vector_t(string *) *holes) {
-    // 生成两个参数的值
-    tac_gen_exp_int(exp->exp_op.arg1);
-    string const a1 = ir_now->dest;
-
-    tac_gen_exp_int(exp->exp_op.arg2);
-    string const a2 = ir_now->dest;
-
-    // 根据关系符的类型来求出什么时候应该跳转到别的部分
-    ir_code_add(IRT_CMP, 0, a1, a2);
-    IR_Type const neg_ir_type = rel_op_to_negative_ir(exp->exp_op.op);
-    vec_add(*holes, ir_code_add_with_undetermined_label(neg_ir_type));
-}
-
-void tac_gen_stmt_if(Ast_Node n) {
+void tac_gen_if(Ast_Node n) {
     assert_type(n, AT_StmtIf);
 
-    Ast_Node const cond = n->stmt_if.cond;
+    vector_t(string *) if_holes   = create_empty_label_hole();
+    vector_t(string *) else_holes = create_empty_label_hole();
+
+    cond_gen_log(n->stmt_if.cond, &if_holes, &else_holes);
     
-    // TODO: init
-    vector_t(string *) if_holes;
-    vector_t(string *) else_holes;
-    
-    switch (cond->type) {
-        case AT_ExpOp: {
-            Ast_OpType const op = cond->exp_op.op;
-            if (is_int(op)) {
-                // 按正常表达式求值并判 0
-                goto gen_exp;
-            } else if (is_relation(op)) {
-                gen_exp_cond_with_jmp(cond, &else_holes);
-            } else if (is_logic(op)) {
-                switch (op) {
-                    case OP_LOG_NOT: 
-                        
-                    case OP_LOG_AND:
-                    case OP_LOG_OR: 
-                    default: panic("Not a logic op type: %d", op);
-                }
-            } else panic("Unknown OP type: %d", op);
+    stmt_gen_and_fill_holes(n->stmt_if.if_clause, if_holes);
+    string *end = ir_code_add_with_undetermined_label(IRT_JMP, 0, 0);
 
-        } break;
+    stmt_gen_and_fill_holes(n->stmt_if.else_clause, else_holes);
+    *end = num2str(ir_now_offset);
 
-        case AT_ExpNum: {
-            int const num = cond->exp_num.val;
-
-            // 直接做死代码优化 跳过正常的两段代码生成
-            tac_gen_stmt(num ? n->stmt_if.if_clause : n->stmt_if.else_clause);
-            goto clear_up;   
-        } break;
-
-        case AT_ExpCall: // fall down
-        case AT_ExpLval: {
-        gen_exp: 
-            // 计算出表达式的值
-            tac_gen_exp_int(n);
-
-            // 比较并生成跳转指令
-            ir_code_add(IRT_CMP, 0, ir_now->dest, num2str(0));
-
-            // 如果等于 0 就直接跳转到 else
-            vec_add(else_holes, ir_code_add_with_undetermined_label(IRT_JEQ));
-        } break;
-
-        
-    }
-
-   gen_stmt_and_fill_holes(n->stmt_if.if_clause, &if_holes);
-   string * const end_hole = ir_code_add_with_undetermined_label(IRT_JMP);
-   gen_stmt_and_fill_holes(n->stmt_if.else_clause, &else_holes);
-   *end_hole = num2str(ir_now_offset + 1);
-
-clear_up:
     vec_destory(if_holes);
     vec_destory(else_holes);
 }
 
-void tac_gen_stmt(Ast_Node n) {
-    assert_type_stmt(n);
 
-    switch (n->type) {
+// void gen_exp_cond_with_jmp(Ast_Node exp, vector_t(string *) *holes) {
+//     // 生成两个参数的值
+//     tac_gen_exp_int(exp->exp_op.arg1);
+//     string const a1 = ir_now->dest;
 
-        case AT_StmtIf: {
+//     tac_gen_exp_int(exp->exp_op.arg2);
+//     string const a2 = ir_now->dest;
 
-        } break;
+//     // 根据关系符的类型来求出什么时候应该跳转到别的部分
+//     ir_code_add(IRT_CMP, 0, a1, a2);
+//     IR_Type const neg_ir_type = rel_op_to_negative_ir(exp->exp_op.op);
+//     vec_add(*holes, ir_code_add_with_undetermined_label(neg_ir_type));
+// }
 
-        case AT_StmtWhile: {
+// void tac_gen_stmt_if(Ast_Node n) {
+//     assert_type(n, AT_StmtIf);
 
-        } break;
+//     Ast_Node const cond = n->stmt_if.cond;
+    
+//     // TODO: init
+//     vector_t(string *) if_holes;
+//     vector_t(string *) else_holes;
+    
+//     switch (cond->type) {
+//         case AT_ExpOp: {
+//             Ast_OpType const op = cond->exp_op.op;
+//             if (is_int(op)) {
+//                 // 按正常表达式求值并判 0
+//                 goto gen_exp;
+//             } else if (is_relation(op)) {
+//                 gen_exp_cond_with_jmp(cond, &else_holes);
+//             } else if (is_logic(op)) {
+//                 switch (op) {
+//                     case OP_LOG_NOT: 
+                        
+//                     case OP_LOG_AND:
+//                     case OP_LOG_OR: 
+//                     default: panic("Not a logic op type: %d", op);
+//                 }
+//             } else panic("Unknown OP type: %d", op);
+
+//         } break;
+
+//         case AT_ExpNum: {
+//             int const num = cond->exp_num.val;
+
+//             // 直接做死代码优化 跳过正常的两段代码生成
+//             tac_gen_stmt(num ? n->stmt_if.if_clause : n->stmt_if.else_clause);
+//             goto clear_up;   
+//         } break;
+
+//         case AT_ExpCall: // fall down
+//         case AT_ExpLval: {
+//         gen_exp: 
+//             // 计算出表达式的值
+//             tac_gen_exp_int(n);
+
+//             // 比较并生成跳转指令
+//             ir_code_add(IRT_CMP, 0, ir_now->dest, num2str(0));
+
+//             // 如果等于 0 就直接跳转到 else
+//             vec_add(else_holes, ir_code_add_with_undetermined_label(IRT_JEQ));
+//         } break;
+
+        
+//     }
+
+//    gen_stmt_and_fill_holes(n->stmt_if.if_clause, &if_holes);
+//    string * const end_hole = ir_code_add_with_undetermined_label(IRT_JMP);
+//    gen_stmt_and_fill_holes(n->stmt_if.else_clause, &else_holes);
+//    *end_hole = num2str(ir_now_offset + 1);
+
+// clear_up:
+//     vec_destory(if_holes);
+//     vec_destory(else_holes);
+// }
+
+// void tac_gen_stmt(Ast_Node n) {
+//     assert_type_stmt(n);
+
+//     switch (n->type) {
+
+//         case AT_StmtIf: {
+
+//         } break;
+
+//         case AT_StmtWhile: {
+
+//         } break;
 
 
-        case AT_StmtBreak: {
+//         case AT_StmtBreak: {
 
-        } break;
+//         } break;
 
-        case AT_StmtContinue: {
+//         case AT_StmtContinue: {
 
-        } break;
+//         } break;
 
-        case AT_StmtEmpty: {
-            // Do nothing;
-        } break;
+//         case AT_StmtEmpty: {
+//             // Do nothing;
+//         } break;
 
-        case AT_Block: { 
-            tac_gen_block(n); 
-        } break; 
+//         case AT_Block: { 
+//             tac_gen_block(n); 
+//         } break; 
 
-        case AT_StmtExp: {
-            tac_gen_exp_int(n->stmt_exp.exp);
-        } break;
+//         case AT_StmtExp: {
+//             tac_gen_exp_int(n->stmt_exp.exp);
+//         } break;
 
-        case AT_StmtReturn: {
-            if (n->stmt_return.exp) {
-                tac_gen_exp_int(n->stmt_return.exp);
-                ir_code_add(IRT_RET, 0, ir_now->dest, 0);
-            } else {
-                ir_code_add(IRT_RET, 0, 0, 0);
-            }
-        } break;
+//         case AT_StmtReturn: {
+//             if (n->stmt_return.exp) {
+//                 tac_gen_exp_int(n->stmt_return.exp);
+//                 ir_code_add(IRT_RET, 0, ir_now->dest, 0);
+//             } else {
+//                 ir_code_add(IRT_RET, 0, 0, 0);
+//             }
+//         } break;
 
-        case AT_StmtAssign: {
-            Ast_Node const lval = n->stmt_assign.lval;
+//         case AT_StmtAssign: {
+//             Ast_Node const lval = n->stmt_assign.lval;
             
-            tac_gen_exp_int(n->stmt_assign.exp);
-            string const exp_name = ir_now->dest;
+//             tac_gen_exp_int(n->stmt_assign.exp);
+//             string const exp_name = ir_now->dest;
 
-            if (lval_is_array(lval)) {
-                tac_gen_lval_addr(lval);
-                ir_code_add(IRT_STORE, lval->lval.name, ir_now->dest, exp_name);
-            } else {
-                ir_code_add(IRT_MOV, lval->lval.name, exp_name, 0);
-            }
+//             if (lval_is_array(lval)) {
+//                 tac_gen_lval_addr(lval);
+//                 ir_code_add(IRT_STORE, lval->lval.name, ir_now->dest, exp_name);
+//             } else {
+//                 ir_code_add(IRT_MOV, lval->lval.name, exp_name, 0);
+//             }
 
-        } break;
+//         } break;
 
-        default: panic("Unsupport type: %d", n->type);
-    }
-}
+//         default: panic("Unsupport type: %d", n->type);
+//     }
+// }
